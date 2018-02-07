8b0f1a3011e97c16c23731ad7e4dde42af895a4c
Alexander Chizhov <adchizhov@mail.ru>
2018-02-07 16:02:55 +0300

    draw with spacing!

--- draw_image/draw.py
+++ draw_image/draw.py
@@ -4,7 +4,8 @@ from PIL import ImageDraw
 import textwrap
 
 
-def draw_on_image(image_path, image_save_path, font_path, text_to_draw, shadowcolor='black', font_size=70):
+def draw_on_image(image_path, image_save_path, font_path, text_to_draw,
+                  shadowcolor='black', spacing=15, font_size=70):
     """
     Рисуем на изображении буквы
     :param image_path: str - Путь до изображения которое открываем
@@ -12,6 +13,7 @@ def draw_on_image(image_path, image_save_path, font_path, text_to_draw, shadowco
     :param font_path: str - Путь до шрифта ttf!
     :param text_to_draw: str - Что пишем собственно говоря
     :param shadowcolor: str - Цвет обводки
+    :param spacing: int - Пробелы между строками
     :param font_size: int - Размер шрифта
     :return:
     """
@@ -30,20 +32,21 @@ def draw_on_image(image_path, image_save_path, font_path, text_to_draw, shadowco
     draw = ImageDraw.Draw(img)
     # Получаем размеры текста
     text_width, text_height = draw.textsize(splitted_text, font=font)
-    # Начальные координаты, текст, цвет rgb, font=font
     # Рисуем цветные буквы немного большего размера
     draw.multiline_text(((img_width-text_width)/2 + 2, (img_height-text_height)/2 + 2),
-                        splitted_text, fill=shadowcolor, font=font, align='center')
+                        splitted_text, fill=shadowcolor, font=font, spacing=spacing, align='center')
     # Рисуем белый текст
     draw.multiline_text(((img_width-text_width)/2, (img_height-text_height)/2),
-                        splitted_text, fill=(255, 255, 255), font=font, align='center')
+                        splitted_text, fill=(255, 255, 255), font=font, spacing=spacing, align='center')
     # И сохраняем его как новое
     img.save(image_save_path)
 
 
 if __name__ == "__main__":
-    draw_on_image(image_path='sample2.jpg',
+    draw_on_image(image_path='sample.jpg',
                   image_save_path='sample_with_text.jpg',
                   font_path='reforma_grotesk_bold.ttf',
                   text_to_draw='АНДРЕЙ ВОРОБЬЕВ ВЫСТУПИТ С ЕЖЕГОДНЫМ ОБРАЩЕНИЕМ К ЖИТЕЛЯМ ПОДМОСКОВЬЯ 14 ФЕВРАЛЯ',
+                  shadowcolor='green',
+                  spacing=20,
                   font_size=60)
