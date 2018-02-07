6553f8045d5aa0c316355a7e736c13893d804c55
Alexander Chizhov <adchizhov@mail.ru>
2018-02-07 15:52:05 +0300

    draw black text under main

--- draw_image/draw.py
+++ draw_image/draw.py
@@ -4,16 +4,18 @@ from PIL import ImageDraw
 import textwrap
 
 
-def draw_on_image(image_path, image_save_path, font_path, text_to_draw, font_size=30):
+def draw_on_image(image_path, image_save_path, font_path, text_to_draw, shadowcolor='black', font_size=70):
     """
     Рисуем на изображении буквы
     :param image_path: str - Путь до изображения которое открываем
     :param image_save_path: str - Путь куда сохраняем
     :param font_path: str - Путь до шрифта ttf!
     :param text_to_draw: str - Что пишем собственно говоря
+    :param shadowcolor: str - Цвет обводки
     :param font_size: int - Размер шрифта
     :return:
     """
+
     # Открываем изображение
     img = Image.open(image_path)
     # Получаем размеры
@@ -29,6 +31,10 @@ def draw_on_image(image_path, image_save_path, font_path, text_to_draw, font_siz
     # Получаем размеры текста
     text_width, text_height = draw.textsize(splitted_text, font=font)
     # Начальные координаты, текст, цвет rgb, font=font
+    # Рисуем цветные буквы немного большего размера
+    draw.multiline_text(((img_width-text_width)/2 + 2, (img_height-text_height)/2 + 2),
+                        splitted_text, fill=shadowcolor, font=font, align='center')
+    # Рисуем белый текст
     draw.multiline_text(((img_width-text_width)/2, (img_height-text_height)/2),
                         splitted_text, fill=(255, 255, 255), font=font, align='center')
     # И сохраняем его как новое
@@ -36,8 +42,8 @@ def draw_on_image(image_path, image_save_path, font_path, text_to_draw, font_siz
 
 
 if __name__ == "__main__":
-    draw_on_image(image_path='sample.jpg',
+    draw_on_image(image_path='sample2.jpg',
                   image_save_path='sample_with_text.jpg',
                   font_path='reforma_grotesk_bold.ttf',
                   text_to_draw='АНДРЕЙ ВОРОБЬЕВ ВЫСТУПИТ С ЕЖЕГОДНЫМ ОБРАЩЕНИЕМ К ЖИТЕЛЯМ ПОДМОСКОВЬЯ 14 ФЕВРАЛЯ',
-                  font_size=70)
+                  font_size=60)
